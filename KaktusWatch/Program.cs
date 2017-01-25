2e00074d4138e50a30cee3e15d22c47c311d9794
Pavel Winkler <winkler@rationis.eu>
2017-01-25 17:56:57 +0100

    init

---
+++ KaktusWatch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Globalization;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+
+namespace KaktusWatch
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+
+        const string kaktusFBUrl = "https://graph.facebook.com/Kaktus/posts?access_token=1672094689755085|671e0538eaaffd57d780c950b713584c";
+        const string fileLocation = "LastSentDate.kaktus";
+
+        static IEnumerable<string> emailRecipients
+            => ((NameValueCollection)ConfigurationManager.GetSection("recipientsSection")).GetValues("address");
+
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+            if (config.IsDevelopment)
+                config.UseDevelopmentSettings();
+
+            //var host = new JobHost();
+            // The following code ensures that the WebJob will be running continuously
+            //host.RunAndBlock();
+
+            string lastSentFile;
+            DateTime lastSent;
+            try
+            {
+                lastSentFile = File.ReadAllText(fileLocation);
+            }
+            catch
+            {
+                lastSentFile = string.Empty;
+            }
+
+            DateTime.TryParse(lastSentFile, CultureInfo.InvariantCulture, DateTimeStyles.None, out lastSent);
+            if (lastSent.Date == DateTime.UtcNow.Date)
+                return;
+
+            var posts = Worker.GetData(kaktusFBUrl).Result;
+
+            if (Worker.AmIWinningLottery(posts))
+            {
+                Worker.SendEmails(emailRecipients);
+                File.WriteAllText(fileLocation, DateTime.UtcNow.ToString(CultureInfo.InvariantCulture));
+            }
+        }
+
+    }
+}
---
+++ KaktusWatch/Worker.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace KaktusWatch
+{
+    public static class Worker
+    {
+
+        public static async Task<FbObject> GetData(string uri)
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(uri);
+            var data = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<FbObject>(data);
+        }
+
+        public static bool AmIWinningLottery(FbObject posts)
+        {
+            if (!(posts?.Data?.Any() ?? false))
+                return false;
+            return posts.Data.Any(IsWinningTicket);
+        }
+
+        static bool IsWinningTicket(Data post)
+            => post.CreatedTime.Date == DateTime.UtcNow.Date &&
+               (post.Message.Contains("www.mujkaktus.cz/chces-pridat") ||
+                post.Message.Contains("kreditnavic"));
+
+        public static void SendEmails(IEnumerable<string> recipients)
+        {
+            foreach (var recipient in recipients)
+            {
+                var mail = new MailMessage("kaktussender@gmail.com", recipient);
+                var client = new SmtpClient
+                {
+                    Port = 587,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Host = "smtp.gmail.com",
+                    EnableSsl = true,
+                    Timeout = 10000,
+                    Credentials = new System.Net.NetworkCredential("kaktussender@gmail.com", "Buddy Kaktus")
+                };
+
+                mail.Subject = "Kaktus watch";
+                mail.Body = "";
+                client.Send(mail);
+            }
+        }
+
+        public static IEnumerable<int> GetPromotionTimeFrame(string message)
+        {
+            foreach (string word in message.Split(' '))
+            {
+                int hour;
+                if (int.TryParse(word, out hour))
+                    yield return hour;
+            }
+        }
+
+    }
+}
