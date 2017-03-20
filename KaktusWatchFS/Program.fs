0c7ec36c0a1549a00ae09c64bc4cfef0ef0ef0f4
Pavel Winkler <winkler@rationis.eu>
2017-03-20 16:47:24 +0100

    fsharp init

---
+++ KaktusWatchFS/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open Worker
+
+
+// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+type FacebookPost = { 
+    Message: string 
+    CreatedTime: DateTime
+    }
+
+ type FacebookFeed = {
+    Data: List<FacebookPost>
+    }
+
+let kaktusFBUrl = "https://graph.facebook.com/Kaktus/posts?access_token=1672094689755085|671e0538eaaffd57d780c950b713584c"
+let triggerTime = 60
+
+let IsPromotion (post:FacebookPost) (triggerInterval:int) =
+    post.CreatedTime >= DateTime.UtcNow - TimeSpan(0, triggerInterval, 0) &&
+    post.Message.Contains("mujkaktus.cz/chces-pridat")
+    
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let jsonData = fetchUrl myCallback kaktusFBUrl
+    printf "%A" jsonData
+    0 // return an integer exit code
---
+++ KaktusWatchFS/Worker.fs
@@ -0,0 +1,27 @@
+﻿module Worker
+
+open System
+open System.Net
+open System.IO
+
+let parseFeed (sr:StreamReader) =
+    0
+
+
+let myCallback (reader:IO.StreamReader) url = 
+    let html = reader.ReadToEnd()
+    let html1000 = html.Substring(0,1000)
+    printfn "Downloaded %s. First 1000 is %s" url html1000
+    html
+
+let fetchUrl callback url =        
+    let req = WebRequest.Create(Uri(url)) 
+    use resp = req.GetResponse() 
+    use stream = resp.GetResponseStream() 
+    use reader = new IO.StreamReader(stream) 
+    callback reader url
+
+let GetData (url:string) =
+    let req = HttpWebRequest.Create(url) :?> HttpWebRequest 
+    req.ProtocolVersion <- HttpVersion.Version10
+    req.Method <- "GET"
