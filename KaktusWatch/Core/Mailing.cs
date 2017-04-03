2ccffb6437d32446669c3579be0826d939335d7d
p.winkler <p.winkler@CREDITI-MRI6NIQ>
2017-04-03 16:36:51 +0200

    hour parsing tuned

--- KaktusWatch/Core/Mailing.cs
+++ KaktusWatch/Core/Mailing.cs
@@ -6,18 +6,18 @@ using SendGrid.Helpers.Mail;
 
 namespace KaktusWatch.Core
 {
-    public class Mailing
+    public static class Mailing
     {
-        public static SendGridAPIClient GetClient()
+        static SendGridAPIClient GetClient()
             => new SendGridAPIClient("SG"+".QB9FWm07SWm9V-Ydd1To5g.kvnnjYKdcIKT18ItzZnDgzB3hj4CYXfsRuw7X_crMK0");
 
         public static async Task SendEmails(IEnumerable<string> recipients, string promotionMessage)
         {
             var sg = GetClient();
             var from = new Email("kaktussender@gmail.com");
-            var subject = string.Concat("Kaktus promotion", GetSubject(GetPromotionTimeFrame(promotionMessage)));
+            var subject = string.Concat("Kaktus promotion", GetSubject(GetPromotionTimeFrame(promotionMessage).ToList()));
             var content = new Content("text/plain", promotionMessage);
-            
+
             foreach (var recipient in recipients)
             {
                 var to = new Email(recipient);
@@ -26,16 +26,19 @@ namespace KaktusWatch.Core
             }
         }
 
-        static string GetSubject(IEnumerable<int> timeFrame)
-            => timeFrame.Count() == 2
-                ? $" - {timeFrame.First()}:00 to {timeFrame.Last()}:00"
+        static string GetSubject(IReadOnlyCollection<string> timeFrame)
+            => timeFrame.Count == 2
+                ? $" - {timeFrame.First()} to {timeFrame.Last()}"
                 : "";
 
-        public static IEnumerable<int> GetPromotionTimeFrame(string message)
+        public static IEnumerable<string> GetPromotionTimeFrame(string message)
         {
-            foreach (string word in message.Split(' '))
-                if (int.TryParse(word.Replace(".", "").Replace(":", ""), out int hour))
-                    yield return hour;
+            bool IsHour(string word)
+                => int.TryParse(word.Replace(".", "").Replace(":", ""), out int hour);
+            
+            return message
+                .Split(' ')
+                .Where(IsHour);
         }
     }
 }
